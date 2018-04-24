983db8babc40b0ac2196898072f54a0455dc20fe
wesleymutwiri <wesleymochiemo@gmail.com>
2018-04-24 15:40:35 +0300

    added comments to the projects

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,25 @@
-
+# imports to be used in the application
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-
+from flask import Blueprint
+# initializing Blueprints
+auth = Blueprint('auth', __name__)
+# defining bootstrap
 bootstrap = Bootstrap()
+# creating the application
 def create_app(config_state):
     app = Flask(__name__)
     app.config.from_object(config_options[config_state])
 
-
+    # bootstrap initialization
     bootstrap.init_app(app)
     # db.init_app(app)
-
+    # registering main blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
-from flask import Blueprint
-auth = Blueprint('auth', __name__)
 
 from . import views
